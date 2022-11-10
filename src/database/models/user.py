ec8a9c2030a47de6515c5606d7a0492aa4f62cbc
Daniel Bailo <danbailoufms@gmail.com>
2022-11-10 13:04:23 -0300

    hotfix: force non nullable values to columns

--- src/database/models/user.py
+++ src/database/models/user.py
@@ -1,12 +1,12 @@
 from sqlalchemy import Boolean, Column, String
-
+from sqlalchemy.sql import false
 
 from .base import AtenaBase
 
 
 class UserModel(AtenaBase):
-    name = Column(String)
-    username = Column(String, unique=True, index=True)
-    email = Column(String, unique=True, index=True)
-    password_hash = Column(String)
-    is_active = Column(Boolean, default=False)
+    name = Column(String, nullable=False)
+    username = Column(String, unique=True, index=True, nullable=False)
+    email = Column(String, unique=True, index=True, nullable=False)
+    password_hash = Column(String, nullable=False)
+    is_active = Column(Boolean, server_default=false(), nullable=False)
