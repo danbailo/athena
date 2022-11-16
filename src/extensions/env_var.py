dd31b2d9858a0c23d5e16f15d5dde7ed6e8e541d
Daniel Bailo <danbailoufms@gmail.com>
2022-11-16 19:39:31 -0300

    refactor: raising exception when variable is not declared in env

--- src/extensions/env_var.py
+++ src/extensions/env_var.py
@@ -3,8 +3,16 @@ import os
 
 from dotenv import load_dotenv
 
+from logger import logger
+
 load_dotenv()
 
 
-def get_env_var(key: str) -> str | None:
-    return os.getenv(key)
+def get_env_var(key: str, raise_exception: bool = False) -> str | None:
+    if not (value := os.getenv(key)) and raise_exception is True:
+        raise ValueError('variable does not exists in env!')
+    elif value is None and raise_exception is False:
+        logger.warning(
+            f'variable not declared in the environment - {key.upper()}'
+        )
+    return value
