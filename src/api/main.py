52b7288e799c960092c88d73e7beef01a06d46f2
Daniel Bailo <danbailoufms@gmail.com>
2022-12-13 19:13:30 -0300

    refactor: splitted api and app structure

--- src/api/main.py
+++ src/api/main.py
@@ -1,31 +1,31 @@
 from fastapi import FastAPI
 
 
-from api.constants.mapped_prefix import MAPPED_ENDPOINT_PREFIX
+from api.constants.mapped_prefix import MAPPED_API_ENDPOINT_PREFIX
 from api.routers import auth_router, user_router
 
 from database.connection import database
 
-app = FastAPI()
+api = FastAPI()
 
 
-@app.on_event("startup")
+@api.on_event("startup")
 async def startup():
     await database.connect()
 
 
-@app.on_event("shutdown")
+@api.on_event("shutdown")
 async def shutdown():
     await database.disconnect()
 
 
-app.include_router(
+api.include_router(
     user_router.router,
-    prefix=MAPPED_ENDPOINT_PREFIX['user'],
+    prefix=MAPPED_API_ENDPOINT_PREFIX['user'],
     tags=['user'],
     responses={404: {'description': 'not found!'}}
 )
-app.include_router(
+api.include_router(
     auth_router.router,
     tags=['auth'],
     responses={404: {'description': 'not found!'}}
