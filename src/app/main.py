88c0d60e647494ed75fd356e89d84ee24292d76e
Daniel Bailo <danbailoufms@gmail.com>
2022-12-15 21:17:38 -0300

    chore: included login and logout router

--- src/app/main.py
+++ src/app/main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
 
-from .routers import home_router
+from .routers import home_router, login_router, logout_router
 
 app = FastAPI()
 app.mount("/static", StaticFiles(directory="app/static"), name="static")
@@ -9,5 +9,16 @@ app.mount("/static", StaticFiles(directory="app/static"), name="static")
 
 app.include_router(
     home_router.router,
+    prefix='/home',
+    responses={404: {'description': 'not found!'}}
+)
+app.include_router(
+    login_router.router,
+    prefix='/login',
+    responses={404: {'description': 'not found!'}}
+)
+app.include_router(
+    logout_router.router,
+    prefix='/logout',
     responses={404: {'description': 'not found!'}}
 )
