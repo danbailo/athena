108166fbcefde69f574727c8d603aa539a5db238
Daniel Bailo <danbailoufms@gmail.com>
2022-12-15 21:15:24 -0300

    refactor: applied method to render template

--- src/app/routers/home_router.py
+++ src/app/routers/home_router.py
@@ -1,11 +1,12 @@
 from fastapi import APIRouter, Request
 from fastapi.responses import HTMLResponse
 
-from .base import templates
+
+from .base_router import render_template
 
 router = APIRouter()
 
 
-@router.get('/', response_class=HTMLResponse)
+@router.get('', response_class=HTMLResponse)
 async def home(request: Request):
-    return templates.TemplateResponse('home.html', {'request': request})
+    return await render_template('home_template.html', request, 200)
