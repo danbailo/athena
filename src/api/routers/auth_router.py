c15c9fa61e22cd96935b48929e02457b9aa78a76
Daniel Bailo <danbailoufms@gmail.com>
2022-12-15 21:17:12 -0300

    style: imports

--- src/api/routers/auth_router.py
+++ src/api/routers/auth_router.py
@@ -14,15 +14,14 @@ from sqlalchemy import select
 from extensions.env_var import get_env_var
 from extensions.exceptions import GetUserError, LoginError, InactiveUserError
 
-from ..database.connection import database
-from ..database.models.user_model import UserModel
-
-from ..serializers.auth_serializer import (
+from serializers.auth_serializer import (
     TokenResponseSerializer, TokenDataSerializer
 )
 
-from ..utils.auth_util import verify_password
+from utils.auth_util import verify_password
 
+from ..database.connection import database
+from ..database.models.user_model import UserModel
 
 SECRET_KEY = get_env_var('SECRET_KEY')
 ALGORITHM = get_env_var('ALGORITHM')
@@ -30,7 +29,7 @@ ACCESS_TOKEN_EXPIRE_MINUTES = 30
 
 router = APIRouter()
 
-oauth2_scheme = OAuth2PasswordBearer(tokenUrl=router.prefix + '/token')
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl=f'{router.prefix}/token')
 
 
 async def get_user_in_db(db: Database, username: str) -> UserModel | None:
