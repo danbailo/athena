d15d3a32dd9d30b30d5c838ea003bfb21f726c84
Daniel Bailo <danbailoufms@gmail.com>
2022-12-15 21:08:44 -0300

    refactor: changed serializers folder to source of project

---
+++ src/serializers/context_serializer.py
@@ -0,0 +1,22 @@
+from typing import Any
+
+from enum import StrEnum
+
+from pydantic import BaseModel
+
+
+class AlertTypeEnum(StrEnum):
+    primary = 'primary'
+    secondary = 'secondary'
+    success = 'success'
+    danger = 'danger'
+    warning = 'warning'
+    info = 'info'
+    light = 'light'
+    dark = 'dark'
+
+
+class ContextSerializer(BaseModel):
+    request: Any
+    msg: str
+    alert_type: AlertTypeEnum = AlertTypeEnum.info
