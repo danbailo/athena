402e87eee41c9f5dd8f39231abe613b317aafe9a
Daniel Bailo <danbailoufms@gmail.com>
2022-12-19 21:26:19 -0300

    feat: class to handle authentication middleware

--- src/serializers/auth_serializer.py
+++ src/serializers/auth_serializer.py
@@ -1,4 +1,13 @@
+from jose import jwt, ExpiredSignatureError, JWTError
+
 from pydantic import BaseModel
+
+from starlette.authentication import (
+    AuthCredentials, AuthenticationBackend, SimpleUser, UnauthenticatedUser
+)
+
+
+from extensions.env_var import get_env_var
 
 
 class TokenResponseSerializer(BaseModel):
@@ -8,3 +17,23 @@ class TokenResponseSerializer(BaseModel):
 
 class TokenDataSerializer(BaseModel):
     username: str | None = None
+
+
+class OAuth2Backend(AuthenticationBackend):
+    async def authenticate(self, conn):
+        if 'access_token' not in conn.headers.get('cookie', ''):
+            return
+        auth = conn.headers['cookie'].split('=')[-1].strip('"')
+        _, token = auth.split()
+        try:
+            payload = jwt.decode(
+                token, get_env_var('SECRET_KEY'),
+                algorithms=[get_env_var('ALGORITHM')]
+            )
+        except ExpiredSignatureError:
+            return\
+                AuthCredentials(["not_authenticated"]), UnauthenticatedUser()
+        except JWTError:
+            return
+        username = payload.get("sub")
+        return AuthCredentials(["authenticated"]), SimpleUser(username)
