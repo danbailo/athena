83d5f5e35954c4a977222e3efd15b869dc4ba5f5
Daniel Bailo <danbailoufms@gmail.com>
2022-12-19 21:23:43 -0300

    chore: added middleware and hangling exceptions

--- src/app/main.py
+++ src/app/main.py
@@ -1,22 +1,29 @@
 from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
 
+from starlette.middleware.authentication import AuthenticationMiddleware
 
-from constants.mapped_prefix import MAPPED_API_ENDPOINT_PREFIX
 
-from .routers import home_router, login_router
+from extensions.exceptions import handle_403_http_error, handle_404_http_error
 
-app = FastAPI()
+from serializers.auth_serializer import OAuth2Backend
+
+from .routers import home_router, user_router
+
+app = FastAPI(docs_url=None, redoc_url=None)
+
+app.add_middleware(AuthenticationMiddleware, backend=OAuth2Backend())
+
+app.add_exception_handler(403, handle_403_http_error)
+app.add_exception_handler(404, handle_404_http_error)
+
 app.mount("/static", StaticFiles(directory="app/static"), name="static")
-
 
 app.include_router(
     home_router.router,
-    prefix='/home',
     responses={404: {'description': 'not found!'}}
 )
 app.include_router(
-    login_router.router,
-    prefix=MAPPED_API_ENDPOINT_PREFIX['user'],
+    user_router.router,
     responses={404: {'description': 'not found!'}}
 )
