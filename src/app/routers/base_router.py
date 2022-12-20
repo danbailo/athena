0617f55103da6d869a4c5e15ae4822923423f39f
Daniel Bailo <danbailoufms@gmail.com>
2022-12-19 21:18:27 -0300

    refactor: removed specific methods

--- src/app/routers/base_router.py
+++ src/app/routers/base_router.py
@@ -1,4 +1,4 @@
-from fastapi import Request
+from fastapi import Request, HTTPException
 from fastapi.templating import Jinja2Templates
 
 
@@ -9,9 +9,11 @@ templates = Jinja2Templates(directory="app/templates")
 
 async def render_template(
     template_name: str, context_request: Request, status_code: int,
-    alert_msg: str | None = None, alert_type: AlertTypeEnum | None = None,
-    headers: dict[str, str] | None = None
+    alert_msg: str = '', alert_type: AlertTypeEnum | None = None,
+    exc: HTTPException | None = None, headers: dict[str, str] | None = None
 ):
+    if exc:
+        alert_msg += exc.detail
     return templates.TemplateResponse(
         template_name,
         ContextSerializer(
@@ -21,21 +23,4 @@ async def render_template(
         ).dict(),
         status_code=status_code,
         headers=headers
-    )
-
-
-async def render_404_error_template(
-    context_request: Request, err: Exception | None = None
-):
-    msg = 'Unexpected error'
-    if err:
-        msg += f' - {err}'
-    return templates.TemplateResponse(
-        '404_template.html',
-        ContextSerializer(
-            request=context_request,
-            msg=msg,
-            alert_type=AlertTypeEnum.danger
-        ).dict(),
-        status_code=404
     )
