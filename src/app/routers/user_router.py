6352d56b232cde5dcba1c79a2a4b827c86bca000
Daniel Bailo <danbailoufms@gmail.com>
2022-12-19 21:17:54 -0300

    feat: router to do user actions and forcing auth

--- src/app/routers/user_router.py
+++ src/app/routers/user_router.py
@@ -3,37 +3,49 @@ from fastapi.responses import HTMLResponse, Response, RedirectResponse
 
 from httpx import HTTPStatusError
 
+from constants.mapped_prefix import MAPPED_API_ENDPOINT_PREFIX
+
 
 from extensions.base_requests import async_fetch, MethodEnum
 
 from serializers.context_serializer import AlertTypeEnum
 
-from .base_router import (
-    templates, render_template, render_404_error_template
-)
+from .base_router import render_template
 
 from ..forms.login_form import LoginForm
 
-router = APIRouter()
+from starlette.authentication import requires
+
+
+router = APIRouter(
+    prefix=MAPPED_API_ENDPOINT_PREFIX['user']
+)
+
+
+@router.get('')
+@requires('authenticated')
+async def user_page(request: Request):
+    return await render_template(
+        'user_template.html', request, 200
+    )
 
 
 @router.get('/login', response_class=HTMLResponse)
 async def login_page(request: Request):
-    return templates.TemplateResponse(
-        'login_template.html',
-        {'request': request}
+    return await render_template(
+        'login_template.html', request, 200
     )
 
 
-@router.post('/login', response_class=HTMLResponse)
+@router.post('/login', response_class=RedirectResponse)
 async def login_user(
     request: Request,
     response: Response,
     form_data: LoginForm = Depends(LoginForm.as_form)
 ):
     try:
-        user_token = await async_fetch(
-            MethodEnum.post, 'http://127.0.0.1:8000/auth/token',
+        data = await async_fetch(
+            MethodEnum.post, 'http://localhost:8000/auth/token',
             headers={
                 'accept': 'application/json',
                 'Content-Type': 'application/x-www-form-urlencoded'
@@ -45,21 +57,26 @@ async def login_user(
             'login_template.html', request, 401,
             'Incorrect Username or Password', AlertTypeEnum.warning,
         )
-    except Exception as err:
-        return await render_404_error_template(request, err)
+    except Exception as exc:
+        return await render_template(
+            '404_error.html', request, 404,
+            'Unexpected error', AlertTypeEnum.danger, exc
+        )
     response.set_cookie(
-        key="access_token", value=user_token['access_token'],
+        key="access_token",
+        value=f'{data["token_type"]} {data["access_token"]}',
         httponly=True
     )
     return RedirectResponse(
-        '/home', 302, response.headers
+        request.base_url, 302, response.headers
     )
 
 
 @router.get('/logout')
-async def logout():
+@requires('authenticated', redirect='login_user')
+async def logout_user(request: Request):
     response = RedirectResponse(
-        '/login', 302
+        router.url_path_for('login_user'), 302
     )
     response.delete_cookie(key='access_token')
     return response
