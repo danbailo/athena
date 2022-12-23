e31c51574517f1efec67411f0f2a6334484cb929
Daniel Bailo <danbailoufms@gmail.com>
2022-12-23 17:13:31 -0300

    refactor: removed oauth2backend

--- src/athena/serializers/auth_serializer.py
+++ src/athena/serializers/auth_serializer.py
@@ -1,13 +1,4 @@
-from jose import jwt, ExpiredSignatureError, JWTError
-
 from pydantic import BaseModel
-
-from starlette.authentication import (
-    AuthCredentials, AuthenticationBackend, SimpleUser
-)
-
-
-from extensions.env_var import get_env_var
 
 
 class TokenResponseSerializer(BaseModel):
@@ -17,20 +8,3 @@ class TokenResponseSerializer(BaseModel):
 
 class TokenDataSerializer(BaseModel):
     username: str | None = None
-
-
-class OAuth2Backend(AuthenticationBackend):
-    async def authenticate(self, conn):
-        if 'access_token' not in conn.headers.get('cookie', ''):
-            return
-        auth = conn.headers['cookie'].split('=')[-1].strip('"')
-        _, token = auth.split()
-        try:
-            payload = jwt.decode(
-                token, get_env_var('SECRET_KEY'),
-                algorithms=[get_env_var('ALGORITHM')]
-            )
-        except (ExpiredSignatureError, JWTError):
-            return
-        username = payload.get('sub')
-        return AuthCredentials(['authenticated']), SimpleUser(username)
