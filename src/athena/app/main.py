c5cc80439c0abc450b0857f23c925b280ed5800d
Daniel Bailo <danbailoufms@gmail.com>
2022-12-23 17:29:39 -0300

    feat: implemented custom middleware to flash messages inside headers

--- src/athena/app/main.py
+++ src/athena/app/main.py
@@ -3,8 +3,8 @@ from fastapi.staticfiles import StaticFiles
 
 from starlette.middleware.authentication import AuthenticationMiddleware
 
-
-from serializers.auth_serializer import OAuth2Backend
+from .middlewares.auth_backend_middleware import OAuth2Backend
+from .middlewares.flash_message_middleware import FlashMessageMiddleware
 
 from .routers import home_router, user_router
 
@@ -14,12 +14,13 @@ from .utils.handle_exceptions_util import (
 
 app = FastAPI(docs_url=None, redoc_url=None)
 
+app.add_middleware(FlashMessageMiddleware)
 app.add_middleware(AuthenticationMiddleware, backend=OAuth2Backend())
 
 app.add_exception_handler(403, handle_403_http_error)
 app.add_exception_handler(404, handle_404_http_error)
 
-app.mount("/static", StaticFiles(directory="app/static"), name="static")
+app.mount('/static', StaticFiles(directory='app/static'), name='static')
 
 app.include_router(home_router.router)
 app.include_router(user_router.router)
---
+++ src/athena/app/middlewares/flash_message_middleware.py
@@ -0,0 +1,26 @@
+class FlashMessageMiddleware:
+    def __init__(self, app):
+        self.app = app
+        self._flash_message = None
+
+    async def __call__(self, scope, receive, send):
+        if scope['type'] != 'http':
+            return await self.app(scope, receive, send)
+
+        async def get_flash_message_from_headers(message):
+            if message['type'] != 'http.response.start':
+                return await send(message)
+
+            if flash_messages := list(filter(
+                lambda x: x[0].decode('utf-8').lower().startswith('x-athena-'),
+                message.get('headers', []))
+            ):
+                self._flash_message = flash_messages[0][1].decode('utf-8')
+            else:
+                self._flash_message = None
+
+            await send(message)
+
+        scope['athena_flash_message'] = self._flash_message
+
+        await self.app(scope, receive, get_flash_message_from_headers)
