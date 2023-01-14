c42751a66612b506fd25316e63807694791e5784
Daniel Bailo <danbailoufms@gmail.com>
2023-01-14 14:22:02 -0300

    feat: added user role in front-end scopes

--- src/athena/app/middlewares/auth_backend_middleware.py
+++ src/athena/app/middlewares/auth_backend_middleware.py
@@ -5,21 +5,54 @@ from starlette.authentication import (
 )
 
 
+from extensions.base_requests import async_fetch, MethodEnum
 from extensions.env_var import get_env_var
+
+from serializers.auth_serializer import TokenRequestHeaders
+
+
+class AthenaUser(SimpleUser):
+    def __init__(
+        self, username: str, name: str, email: str,
+        *args, **kwargs
+    ) -> None:
+        self._username = username
+        self._name = name
+        self._email = email
+
+    @property
+    def username(self) -> str:
+        return self._username
+
+    @property
+    def name(self) -> str:
+        return self._name
+
+    @property
+    def email(self) -> str:
+        return self._email
 
 
 class OAuth2Backend(AuthenticationBackend):
     async def authenticate(self, conn):
         if 'access_token' not in conn.headers.get('cookie', ''):
             return
-        auth = conn.headers['cookie'].split('=')[-1].strip('"')
-        _, token = auth.split()
+        auth = TokenRequestHeaders(access_token=conn.headers['cookie'])
+        _, token = auth.access_token.split()
         try:
-            payload = jwt.decode(
+            jwt.decode(
                 token, get_env_var('SECRET_KEY'),
                 algorithms=[get_env_var('ALGORITHM')]
             )
         except (ExpiredSignatureError, JWTError):
             return
-        username = payload.get('sub')
-        return AuthCredentials(['authenticated']), SimpleUser(username)
+
+        url = get_env_var("ATHENA_API_BASE_URL", raise_exception=True)
+        response = await async_fetch(
+            MethodEnum.post, f'{url}/auth/user/info', headers=auth
+        )
+        conn.scope["athena_user_is_admin"] = response['role'] == 'admin'
+        return (
+            AuthCredentials(['authenticated', response['role']]),
+            AthenaUser(**response)
+        )
--- src/athena/serializers/auth_serializer.py
+++ src/athena/serializers/auth_serializer.py
@@ -1,4 +1,6 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, Field, validator
+
+import regex
 
 
 class TokenResponseSerializer(BaseModel):
@@ -8,3 +10,18 @@ class TokenResponseSerializer(BaseModel):
 
 class TokenDataSerializer(BaseModel):
     username: str | None = None
+
+
+class TokenRequestHeaders(BaseModel):
+    access_token: str = Field(..., alias='Authorization')
+
+    @validator('access_token')
+    @classmethod
+    def clean_authorization_token(cls, value: str) -> str:
+        return regex.search(
+            r'\baccess_token=\"(?P<token>.+?)\";?',
+            value, regex.I | regex.S
+        )['token']
+
+    class Config:
+        allow_population_by_field_name = True
