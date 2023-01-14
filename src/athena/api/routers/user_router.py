cc47aa1e852cb6dbec2a937f09b779b372e44efe
Daniel Bailo <danbailoufms@gmail.com>
2023-01-14 15:52:58 -0300

    refactor: removed unused resourcers

--- src/athena/api/routers/user_router.py
+++ src/athena/api/routers/user_router.py
@@ -1,75 +1,41 @@
 from fastapi import APIRouter, Depends, status
 
-from sqlalchemy import select, insert, update
+from sqlalchemy import insert, update
 
-
-from extensions.exceptions import (
-    NotAuthorizedError, NothingToPatchError, UserNotFoundError
-)
-from extensions.logger import logger
 
 from serializers.user_serializer import (
-    CreateUserBody, GetUserBody, PatchUserBody
+    CreateUserRequestBody, UserResponseBody, PatchUserRequestBody
 )
 
-from .auth_router import async_get_current_active_user
+from .auth_router import async_get_current_user
 
 from ..database.connection import database
 from ..database.models.user_model import UserModel
 
+from ..utils.exceptions import NothingToPatchError
 
 router = APIRouter()
 
 
-class RoleChecker:
-    def __init__(self, allowed_roles):
-        self.allowed_roles = allowed_roles
-
-    def __call__(
-        self, user: UserModel = Depends(async_get_current_active_user)
-    ):
-        if user.role not in self.allowed_roles:
-            logger.debug(
-                f'User with role {user.role} not in {self.allowed_roles}'
-            )
-            raise NotAuthorizedError()
+@router.post('/detail', response_model=UserResponseBody)
+async def user_detail(
+    current_user: UserModel = Depends(async_get_current_user)
+):
+    return current_user
 
 
-@router.get('', response_model=list[GetUserBody])
-async def get_users(username: str | None = None):
-    if username:
-        query = select(UserModel).where(UserModel.username == username)
-    else:
-        query = select(UserModel)
-    return await database.fetch_all(query)
-
-
-@router.post('', response_model=CreateUserBody)
-async def create_user(user: CreateUserBody):
+@router.post('', response_model=CreateUserRequestBody)
+async def create_user(user: CreateUserRequestBody):
     query = insert(UserModel).values(**user.dict(by_alias=False))
     await database.execute(query)
     return user
 
 
-@router.get("/me", response_model=GetUserBody)
-async def read_users_me(
-    current_user: UserModel = Depends(async_get_current_active_user)
+@router.patch('', status_code=status.HTTP_204_NO_CONTENT)
+async def update_own_info(
+    user_body: PatchUserRequestBody,
+    user: UserModel = Depends(async_get_current_user),
 ):
-    return current_user
-
-
-@router.get("/me/items/")
-async def read_own_items(
-    current_user: UserModel = Depends(async_get_current_active_user)
-):
-    return [{"item_id": "Foo", "owner": current_user.username}]
-
-
-@router.patch('/{username}', status_code=status.HTTP_204_NO_CONTENT)
-async def patch_user(username: str, user_body: PatchUserBody):
-    query = select(UserModel).where(UserModel.username == username)
-    if not (user := await database.fetch_one(query)):
-        raise UserNotFoundError()
     data = user_body.dict(exclude_none=True)
     if not data:
         raise NothingToPatchError()
@@ -77,8 +43,3 @@ async def patch_user(username: str, user_body: PatchUserBody):
         .where(UserModel.id == user.id)\
         .values(**data)
     await database.execute(query)
-
-
-@router.get('/admin', dependencies=[Depends(RoleChecker(['admin']))])
-async def only_admin(user: UserModel = Depends(async_get_current_active_user)):
-    return {'message': f'{user.name} is admin!'}
