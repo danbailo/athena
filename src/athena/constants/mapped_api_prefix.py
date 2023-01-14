f4ff7a6f743fd74eff6a6059b21d1c8514b04aba
Daniel Bailo <danbailoufms@gmail.com>
2023-01-14 16:23:50 -0300

    refactor: module name

--- src/athena/constants/mapped_api_prefix.py
+++ src/athena/constants/mapped_api_prefix.py
@@ -1,7 +1,7 @@
-from enum import Enum
+from enum import StrEnum
 
 
-class EndPointEnum(str, Enum):
+class EndPointEnum(StrEnum):
     user = 'user'
     auth = 'auth'
     home = 'home'
@@ -12,5 +12,5 @@ MAPPED_API_ENDPOINT_PREFIX = {
     EndPointEnum.admin: '/admin',
     EndPointEnum.auth: '/auth',
     EndPointEnum.user: '/user',
-    EndPointEnum.home: ''
+    EndPointEnum.home: '/'
 }
