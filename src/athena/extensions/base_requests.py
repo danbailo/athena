abd12bcff122d653c8020872aab1d569aeabd3aa
Daniel Bailo <danbailoufms@gmail.com>
2023-01-14 14:27:49 -0300

    chore: cookies and headers can be basemodels

--- src/athena/extensions/base_requests.py
+++ src/athena/extensions/base_requests.py
@@ -7,6 +7,7 @@ from httpx import AsyncClient
 
 from pydantic import BaseModel
 
+
 class MethodEnum(StrEnum):
     get = 'get'
     post = 'post'
@@ -17,20 +18,25 @@ class MethodEnum(StrEnum):
 # TODO: Apply tenancity
 async def async_fetch(
     method: MethodEnum, url: str,
-    data: dict[str, Any] | BaseModel | None = None,
-    json: dict[str, Any] | BaseModel | None = None,
     cookies: dict[str, Any] | None = None,
-    headers={'Content-Type': 'application/json'}
+    data: dict[str, Any] | BaseModel | None = None,
+    headers: dict[str, Any] | BaseModel = {'Content-Type': 'application/json'},
+    json: dict[str, Any] | BaseModel | None = None,
+    *args, **kwargs
 ):
+    if isinstance(cookies, BaseModel):
+        cookies = cookies.dict(by_alias=True)
     if isinstance(data, BaseModel):
         data = data.dict(by_alias=True)
+    if isinstance(headers, BaseModel):
+        headers = headers.dict(by_alias=True)
     if isinstance(json, BaseModel):
         json = json.dict(by_alias=True)
     async with AsyncClient() as client:
         response = await client.request(
             method, url, data=data,
             json=json, cookies=cookies,
-            headers=headers
+            headers=headers, *args, **kwargs
         )
         response.raise_for_status()
     return response.json()
