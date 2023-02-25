b8aa91fd5424b1a02a438513e2df59cc33d0f518
Daniel Bailo <danbailoufms@gmail.com>
2023-02-25 12:39:57 -0300

    refactor: raise exception true as default

--- src/athena/extensions/env_var.py
+++ src/athena/extensions/env_var.py
@@ -8,7 +8,7 @@ from .logger import logger
 load_dotenv()
 
 
-def get_env_var(key: str, raise_exception: bool = False) -> str | None:
+def get_env_var(key: str, raise_exception: bool = True) -> str | None:
     if not (value := os.getenv(key)) and raise_exception is True:
         raise ValueError('variable does not exists in env!')
     elif value is None and raise_exception is False:
