054570c861eaec46e4fed298dabe3d9282fc2083
Daniel Bailo <danbailoufms@gmail.com>
2023-05-01 11:40:10 -0300

    refactor: using bs scrollspy

--- src/athena/app/static/js/scripts.js
+++ src/athena/app/static/js/scripts.js
@@ -7,51 +7,52 @@
 // Scripts
 // 
 
-// window.addEventListener('DOMContentLoaded', event => {
+window.addEventListener('DOMContentLoaded', event => {
 
-//     // Navbar shrink function
-//     var navbarShrink = function () {
-//         const navbarCollapsible = document.body.querySelector('#mainNav');
-//         if (!navbarCollapsible) {
-//             return;
-//         }
-//         if (window.scrollY === 0) {
-//             navbarCollapsible.classList.remove('navbar-shrink')
-//         } else {
-//             navbarCollapsible.classList.add('navbar-shrink')
-//         }
+    // // Navbar shrink function
+    // var navbarShrink = function () {
+    //     const navbarCollapsible = document.body.querySelector('#mainNav');
+    //     if (!navbarCollapsible) {
+    //         return;
+    //     }
+    //     if (window.scrollY === 0) {
+    //         navbarCollapsible.classList.remove('navbar-shrink')
+    //     } else {
+    //         navbarCollapsible.classList.add('navbar-shrink')
+    //     }
 
-//     };
+    // };
 
-//     // Shrink the navbar 
-//     navbarShrink();
+    // // Shrink the navbar 
+    // navbarShrink();
 
-//     // Shrink the navbar when page is scrolled
-//     document.addEventListener('scroll', navbarShrink);
+    // // Shrink the navbar when page is scrolled
+    // document.addEventListener('scroll', navbarShrink);
 
-//     // Activate Bootstrap scrollspy on the main nav element
-//     const mainNav = document.body.querySelector('#mainNav');
-//     if (mainNav) {
-//         new bootstrap.ScrollSpy(document.body, {
-//             target: '#mainNav',
-//             offset: 74,
-//         });
-//     };
+    // Activate Bootstrap scrollspy on the main nav element
+    const mainNav = document.body.querySelector('#mainNav');
 
-//     // Collapse responsive navbar when toggler is visible
-//     const navbarToggler = document.body.querySelector('.navbar-toggler');
-//     const responsiveNavItems = [].slice.call(
-//         document.querySelectorAll('#navbarResponsive .nav-link')
-//     );
-//     responsiveNavItems.map(function (responsiveNavItem) {
-//         responsiveNavItem.addEventListener('click', () => {
-//             if (window.getComputedStyle(navbarToggler).display !== 'none') {
-//                 navbarToggler.click();
-//             }
-//         });
-//     });
+    if (mainNav) {
+        new bootstrap.ScrollSpy(document.body, {
+            target: '#mainNav',
+            offset: 90,
+        });
+     }; 
 
-// });
+    // Collapse responsive navbar when toggler is visible
+    const navbarToggler = document.body.querySelector('.navbar-toggler');
+    const responsiveNavItems = [].slice.call(
+        document.querySelectorAll('#navbarResponsive .nav-link')
+    );
+    responsiveNavItems.map(function (responsiveNavItem) {
+        responsiveNavItem.addEventListener('click', () => {
+            if (window.getComputedStyle(navbarToggler).display !== 'none') {
+                navbarToggler.click();
+            }
+        });
+    });
+
+});
 
 // back to top
 //Get the button
@@ -87,7 +88,6 @@ MAPPED_ITEM_NAME_BY_ID = {
     'section-body': 'body',
 
     'user-name': 'name',
-    'user-username': 'username',
     'user-password': 'password',
     'user-email': 'email',
 }
