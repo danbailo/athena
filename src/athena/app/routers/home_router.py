c7a43d6134f0b93671480132dea85d77f14e7a3f
Daniel Bailo <danbailoufms@gmail.com>
2023-05-01 11:40:33 -0300

    feat: returning sections/subsections to index page

--- src/athena/app/routers/home_router.py
+++ src/athena/app/routers/home_router.py
@@ -16,20 +16,35 @@ router = APIRouter(prefix=MAPPED_API_ENDPOINT_PREFIX[EndPointEnum.home])
 
 @router.get('/', response_class=HTMLResponse)
 async def home_page(request: Request):
-    about_section = 'sobre'
     url = get_env_var("ATHENA_API_BASE_URL")
-    if (about := await async_fetch(
+
+    about_section_slug = 'sobre'
+    if (about_section := await async_fetch(
         MethodEnum.get, f'{url}/section',
-        params={'title_slug': about_section}
-    )) and about.is_success:
-        about = about.json()[0]
+        params={'title_slug': about_section_slug}
+    )) and about_section.is_success:
+        about_section = about_section.json()[0]
     if (about_subsections := await async_fetch(
-        MethodEnum.get, f'{url}/section/{about_section}/subsection',
+        MethodEnum.get, f'{url}/section/{about_section_slug}/subsection',
     )) and about_subsections.is_success:
         about_subsections = about_subsections.json()
 
+    for_devs_section_slug = 'para-desenvolvedores'
+    if (for_devs_section := await async_fetch(
+        MethodEnum.get, f'{url}/section',
+        params={'title_slug': for_devs_section_slug}
+    )) and for_devs_section.is_success:
+        for_devs_section = for_devs_section.json()[0]
+    if (for_devs_subsections := await async_fetch(
+        MethodEnum.get, f'{url}/section/{for_devs_section_slug}/subsection',
+    )) and for_devs_subsections.is_success:
+        for_devs_subsections = for_devs_subsections.json()
+
     return await async_render_template(
         'index_template.html', request,
-        context_request={'about': about,
-                         'about_subsections': about_subsections}
-    )
+        context_request={
+            'about_section': about_section,
+            'about_subsections': about_subsections,
+            'for_devs_section': for_devs_section,
+            'for_devs_subsections': for_devs_subsections
+        })
