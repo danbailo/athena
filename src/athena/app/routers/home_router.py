966f094a0fbdbdf7b405ca3e10417576494aefb4
Daniel Bailo <danbailoufms@gmail.com>
2023-05-03 20:41:21 -0300

    fix: h2 section was breaking up css. Improved logic to solve it and refactor some things about this section

--- src/athena/app/routers/home_router.py
+++ src/athena/app/routers/home_router.py
@@ -17,34 +17,35 @@ router = APIRouter(prefix=MAPPED_API_ENDPOINT_PREFIX[EndPointEnum.home])
 @router.get('/', response_class=HTMLResponse)
 async def home_page(request: Request):
     url = get_env_var("ATHENA_API_BASE_URL")
+    # TODO: melhorar rotas e respostas da api
 
     about_section_slug = 'sobre'
-    if (about_section := await async_fetch(
+    if (response := await async_fetch(
         MethodEnum.get, f'{url}/section',
         params={'title_slug': about_section_slug}
-    )) and about_section.is_success:
-        about_section = about_section.json()[0]
-    if (about_subsections := await async_fetch(
+    )) and response.is_success and (about_section := response.json()):
+        about_section = about_section[0]
+    if (response := await async_fetch(
         MethodEnum.get, f'{url}/section/{about_section_slug}/subsection',
-    )) and about_subsections.is_success:
-        about_subsections = about_subsections.json()
+    )) and response.is_success:
+        about_subsections = response.json()
 
-    for_devs_section_slug = 'para-desenvolvedores'
-    if (for_devs_section := await async_fetch(
+    devs_section_slug = 'devs'
+    if (response := await async_fetch(
         MethodEnum.get, f'{url}/section',
-        params={'title_slug': for_devs_section_slug}
-    )) and for_devs_section.is_success:
-        for_devs_section = for_devs_section.json()[0]
-    if (for_devs_subsections := await async_fetch(
-        MethodEnum.get, f'{url}/section/{for_devs_section_slug}/subsection',
-    )) and for_devs_subsections.is_success:
-        for_devs_subsections = for_devs_subsections.json()
+        params={'title_slug': devs_section_slug}
+    )) and response.is_success and (devs_section := response.json()):
+        devs_section = devs_section[0]
+    if (response := await async_fetch(
+        MethodEnum.get, f'{url}/section/{devs_section_slug}/subsection',
+    )) and response.is_success:
+        devs_subsections = response.json()
 
     return await async_render_template(
         'index_template.html', request,
         context_request={
             'about_section': about_section,
             'about_subsections': about_subsections,
-            'for_devs_section': for_devs_section,
-            'for_devs_subsections': for_devs_subsections
+            'devs_section': devs_section,
+            'devs_subsections': devs_subsections
         })
