e17250a21455e8638e3d490ebe5a78874a9e3b1b
Daniel Bailo <danbailoufms@gmail.com>
2023-05-03 20:06:14 -0300

    style: pylint

--- src/athena/serializers/section_serializer.py
+++ src/athena/serializers/section_serializer.py
@@ -14,8 +14,8 @@ class CreateSectionRequestBody(BaseModel):
     title: str
     body: str
 
-    @root_validator
     @classmethod
+    @root_validator
     def set_title_slug(cls, root: dict[str, str]):
         if not root.get('title'):
             return root
@@ -27,16 +27,16 @@ class PatchSectionRequestBody(BaseModel):
     title: str | None = None
     body: str | None = None
 
-    @root_validator
     @classmethod
+    @root_validator
     def set_title_slug(cls, root: dict[str, str]):
         if not root.get('title'):
             return root
         root['title_slug'] = slugify(root['title'])
         return root
 
-    @root_validator
     @classmethod
+    @root_validator
     def set_last_updated(cls, root: dict[str, str]):
         if any(root.values()):
             root['last_updated'] = datetime.utcnow()
@@ -54,8 +54,8 @@ class CreateSubSectionRequestBody(BaseModel):
     sub_body: str = Field(..., alias='sub_section_body')
     sub_link_image: str | None = Field(None, alias='sub_section_link_image')
 
-    @root_validator
     @classmethod
+    @root_validator
     def set_sub_title_slug(cls, root: dict[str, str]):
         if not root.get('sub_title'):
             return root
@@ -71,16 +71,16 @@ class PatchSubSectionRequestBody(BaseModel):
     sub_body: str | None = Field(None, alias='sub_section_body')
     sub_link_image: str | None = Field(None, alias='sub_section_link_image')
 
-    @root_validator
     @classmethod
+    @root_validator
     def set_title_slug(cls, root: dict[str, str]):
         if not root.get('sub_title'):
             return root
         root['sub_title_slug'] = slugify(root['sub_title'])
         return root
 
-    @root_validator
     @classmethod
+    @root_validator
     def set_last_updated(cls, root: dict[str, str]):
         if any(root.values()):
             root['sub_last_updated'] = datetime.utcnow()
