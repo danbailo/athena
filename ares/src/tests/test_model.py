140fdfaf1bef92cbeed45d76f1a2f7d77377b544
Daniel Bailo <danbailoufms@gmail.com>
2023-06-10 15:34:12 -0300

    build: running tests

--- ares/src/tests/test_model.py
+++ ares/src/tests/test_model.py
@@ -12,5 +12,5 @@ class TestModel(TestCase):
                 pass
 
     def test_correct_model_name(self):
-        class ErrorModel(AthenaBase):
+        class ErrorMoadel(AthenaBase):
             pass
