6884e068051bd6d4241b78720e83912c7b815c2d
Daniel Bailo <danbailoufms@gmail.com>
2023-06-18 10:19:03 -0300

    feat: method to set project env vars

--- ares/src/extensions/env_var.py
+++ ares/src/extensions/env_var.py
@@ -1,5 +1,7 @@
 import os
 
+from typing import Any
+
 
 from dotenv import load_dotenv
 
@@ -8,10 +10,20 @@ from .logger import logger
 load_dotenv()
 
 
+ATHENA_PROJECT_VARS = {}
+
+
+def set_project_var(key: Any, value: Any) -> Any:
+    ATHENA_PROJECT_VARS[key] = value
+    return value
+
+
 def get_env_var(key: str, raise_exception: bool = True) -> str | None:
-    if not (value := os.getenv(key)) and raise_exception is True:
+    if not (value := (
+        os.environ.get(key) or ATHENA_PROJECT_VARS.get(key))
+    ) and raise_exception is True and value is None:
         raise ValueError('variable does not exists in env!')
-    elif value is None and raise_exception is False:
+    if value is None and raise_exception is False:
         logger.warning(
             'variable not declared in the environment - %s', key.upper()
         )
