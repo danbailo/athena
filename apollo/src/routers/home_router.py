f687edb865751d4c78ba0f1830bcac3c216690ff
Daniel Bailo <danbailoufms@gmail.com>
2023-06-22 10:29:25 -0300

    wip: static sections

--- apollo/src/routers/home_router.py
+++ apollo/src/routers/home_router.py
@@ -5,27 +5,26 @@ from fastapi.responses import HTMLResponse
 from extensions.base_requests import async_fetch, MethodEnum
 from extensions.env_var import get_env_var
 
-from .base_router import async_render_template
+from .base_router import async_render_template, init_section, init_subsection
 
 router = APIRouter()
 
 
 @router.get('/', response_class=HTMLResponse)
 async def home_page(request: Request):
-    url = get_env_var("ATHENA_ARES_BASE_URL")
-    # TODO: melhorar rotas e respostas da api
+    url = get_env_var('ATHENA_ARES_BASE_URL')
 
-    about_section = []
-    about_subsections = []
-    devs_section = []
-    devs_subsections = []
+    about_section = await init_section('Sobre')
+    about_subsections = await init_subsection()
+
+    devs_section = await init_section('Devs')
+    devs_subsections = await init_subsection()
 
     about_section_slug = 'sobre'
     if (response := await async_fetch(
-        MethodEnum.get, f'{url}/section',
-        params={'title_slug': about_section_slug}
-    )) and response.is_success and (about_section := response.json()):
-        about_section = about_section[0]
+        MethodEnum.get, f'{url}/section/{about_section_slug}'
+    )) and response.is_success:
+        about_section = response.json()
     if (response := await async_fetch(
         MethodEnum.get, f'{url}/section/{about_section_slug}/subsection',
     )) and response.is_success:
@@ -33,10 +32,9 @@ async def home_page(request: Request):
 
     devs_section_slug = 'devs'
     if (response := await async_fetch(
-        MethodEnum.get, f'{url}/section',
-        params={'title_slug': devs_section_slug}
-    )) and response.is_success and (devs_section := response.json()):
-        devs_section = devs_section[0]
+        MethodEnum.get, f'{url}/section/{devs_section_slug}'
+    )) and response.is_success:
+        devs_section = response.json()
     if (response := await async_fetch(
         MethodEnum.get, f'{url}/section/{devs_section_slug}/subsection',
     )) and response.is_success:
