5a0b588d7f6ebdf18c1e75d0bddbf01676ff9604
Daniel Bailo <danbailoufms@gmail.com>
2023-06-23 16:00:06 -0300

    feat: implemented sync method to fetch requests

--- apollo/src/extensions/base_requests.py
+++ apollo/src/extensions/base_requests.py
@@ -7,7 +7,7 @@ from typing import Any
 
 from tenacity import retry, stop_after_attempt, wait_fixed, before_log
 
-from httpx import AsyncClient, Response
+from httpx import AsyncClient, Response, request
 
 
 from pydantic import BaseModel
@@ -50,11 +50,48 @@ async def async_fetch(
         cookies = cookies.dict(by_alias=True)
     if isinstance(headers, BaseModel):
         headers = headers.dict(by_alias=True)
-    async with AsyncClient() as client:
+    async with AsyncClient(timeout=180) as client:
         response = await client.request(
             method, url, data=data, json=json, params=params,
             cookies=cookies, headers=headers, *args, **kwargs
         )
+    if raise_for_status is True:
+        response.raise_for_status()
+    return response
+
+
+@retry(
+    reraise=True,
+    stop=stop_after_attempt(3),
+    wait=wait_fixed(1),
+    before=before_log(logger, logging.DEBUG),
+)
+def _fetch(
+    method: MethodEnum, url: str,
+    data: dict[str, Any] | BaseModel | None = None,
+    json: dict[str, Any] | BaseModel | None = None,
+    params: dict[str, Any] | BaseModel | None = None,
+    cookies: dict[str, Any] | None = None,
+    headers: dict[str, Any] | BaseModel = None,
+    raise_for_status: bool = False,
+    *args, **kwargs
+) -> Response:
+    if headers is None:
+        headers = {'Content-Type': 'application/json'}
+    if isinstance(data, BaseModel):
+        data = data.dict(by_alias=True)
+    if isinstance(json, BaseModel):
+        json = json.dict(by_alias=True)
+    if isinstance(params, BaseModel):
+        params = params.dict(by_alias=True)
+    if isinstance(cookies, BaseModel):
+        cookies = cookies.dict(by_alias=True)
+    if isinstance(headers, BaseModel):
+        headers = headers.dict(by_alias=True)
+    response = request(
+        method, url, data=data, json=json, params=params,
+        cookies=cookies, headers=headers, *args, **kwargs, timeout=180
+    )
     if raise_for_status is True:
         response.raise_for_status()
     return response
