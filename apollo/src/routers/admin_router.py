bac009395177fefc849102566d9d41114dfbaba7
Daniel Bailo <danbailoufms@gmail.com>
2023-06-26 19:01:07 -0300

    feat: implemented routes to handle subsections

--- apollo/src/routers/admin_router.py
+++ apollo/src/routers/admin_router.py
@@ -9,12 +9,15 @@ from starlette.authentication import requires
 from extensions.env_var import get_env_var
 from extensions.base_requests import async_fetch, MethodEnum
 
+
+from forms.section_form import (
+    CreateSectionForm, UpdateSectionForm, UpdateSubSectionForm,
+    CreateSubSectionForm
+)
+
 from serializers.auth_serializer import TokenRequestHeaders
 
 from .base_router import AlertTypeEnum, flash, async_render_template
-
-from forms.section_form import CreateSectionForm, UpdateSectionForm
-
 
 router = APIRouter(prefix='/admin')
 
@@ -79,7 +82,7 @@ async def admin_user_detail_page(request: Request, id: int):
     )
 
 
-@router.post('/user/delete/{id}')
+@router.post('/user/{id}/delete')
 @requires('admin')
 async def admin_delete_user(request: Request, id: int):
     data = {}
@@ -142,7 +145,7 @@ async def admin_section_page(request: Request, page: int = 1, limit: int = 5):
         'admin/admin_section_template.html', request,
         context_request={
             'data': data,
-            'create_section_form': CreateSectionForm.get_form_values(),
+            'create_section_form': CreateSectionForm.get_form_fields(),
             'count': count,
             'limit': limit}
     )
@@ -179,7 +182,9 @@ async def admin_create_section(
 
 @router.get('/section/{id}')
 @requires('admin')
-async def admin_section_detail_page(request: Request, id: int):
+async def admin_section_detail_page(
+    request: Request, id: int, page: int = 1, limit: int = 5
+):
     data = {}
     base_url = get_env_var('ATHENA_ARES_BASE_URL')
     try:
@@ -188,10 +193,23 @@ async def admin_section_detail_page(request: Request, id: int):
             f'{base_url}/section/{id}',
             headers=TokenRequestHeaders(
                 access_token=request.headers['cookie']
-            )
+            ), raise_for_status=True
         )
-        response.raise_for_status()
         data = response.json()
+
+        response = await async_fetch(
+            MethodEnum.get,
+            f'{base_url}/section/{id}/subsection',
+            params={'page': page, 'limit': limit},
+            headers=TokenRequestHeaders(
+                access_token=request.headers['cookie']
+            ), raise_for_status=True
+        )
+        data.update({'subsections': response.json()})
+        response = await async_fetch(
+            MethodEnum.get, f'{base_url}/section/{id}/subsection/count')
+        response.raise_for_status()
+        count = response.json()
     except HTTPStatusError:
         await flash(request, data.get('detail') or '', AlertTypeEnum.danger)
         return await async_render_template(
@@ -204,11 +222,15 @@ async def admin_section_detail_page(request: Request, id: int):
         )
     return await async_render_template(
         'admin/admin_section_detail_template.html', request,
-        context_request={'data': data}
+        context_request={
+            'data': UpdateSectionForm(**data).get_form_values(),
+            'create_subsection_form': CreateSubSectionForm.get_form_fields(),
+            'count': count,
+            'limit': limit}
     )
 
 
-@router.post('/section/patch/{id}')
+@router.post('/section/{id}/patch')
 @requires('admin')
 async def admin_patch_section(
     request: Request,
@@ -241,3 +263,175 @@ async def admin_patch_section(
         )
     return RedirectResponse(
         request.url_for('admin_section_detail_page', id=id), status_code=302)
+
+
+@router.post('/section/{id}/delete')
+@requires('admin')
+async def admin_delete_section(request: Request, id: int):
+    base_url = get_env_var('ATHENA_ARES_BASE_URL')
+    try:
+        response = await async_fetch(
+            MethodEnum.delete,
+            f'{base_url}/admin/section/{id}',
+            headers=TokenRequestHeaders(
+                access_token=request.headers['cookie']
+            )
+        )
+        response.raise_for_status()
+    except HTTPStatusError:
+        if response.content.decode():
+            data = response.json()
+        await flash(request, data.get('detail') or '', AlertTypeEnum.danger)
+        return RedirectResponse(
+            request.url_for('admin_section_page'),
+            status_code=302)
+    except Exception as exc:
+        await flash(request, f'Unexpected error! - {str(exc)}', 'danger')
+        return await async_render_template(
+            'errors/404_error.html', request, 404,
+        )
+    return RedirectResponse(
+        request.url_for('admin_section_page'), status_code=302)
+
+
+@router.get('/section/{section_id}/subsection/{subsection_id}')
+@requires('admin')
+async def admin_subsection_detail_page(
+    request: Request, section_id: int, subsection_id: int
+):
+    data = {}
+    base_url = get_env_var('ATHENA_ARES_BASE_URL')
+    try:
+        response = await async_fetch(
+            MethodEnum.get,
+            f'{base_url}/section/{section_id}/subsection/{subsection_id}',
+            headers=TokenRequestHeaders(
+                access_token=request.headers['cookie']
+            )
+        )
+        response.raise_for_status()
+        data = response.json()
+    except HTTPStatusError:
+        await flash(request, data.get('detail') or '', AlertTypeEnum.danger)
+        return await async_render_template(
+            'errors/403_error.html', request, 403,
+        )
+    except Exception as exc:
+        await flash(request, f'Unexpected error! - {str(exc)}', 'danger')
+        return await async_render_template(
+            'errors/404_error.html', request, 404,
+        )
+    return await async_render_template(
+        'admin/admin_subsection_detail_template.html', request,
+        context_request={
+            'data': UpdateSubSectionForm(**data).get_form_values(),
+        }
+    )
+
+
+@router.post('/section/{section_id}/subsection/create',
+             response_class=HTMLResponse | RedirectResponse)
+@requires('admin')
+async def admin_create_subsection(
+    request: Request,
+    section_id: int,
+    form_data: CreateSubSectionForm = Depends(CreateSubSectionForm.as_form)
+):
+    base_url = get_env_var('ATHENA_ARES_BASE_URL')
+    try:
+        _response = await async_fetch(
+            MethodEnum.post,
+            url=f'{base_url}/admin/section/{section_id}/subsection',
+            json=form_data,
+            headers=TokenRequestHeaders(
+                access_token=request.headers['cookie']
+            )
+        )
+        _response.raise_for_status()
+    except HTTPStatusError:
+        await flash(request, _response.json()['detail'], 'danger')
+        return RedirectResponse(request.url_for(
+            'admin_section_detail_page', id=section_id
+        ), status_code=302)
+    except Exception as exc:
+        await flash(request, f'Unexpected error! - {str(exc)}', 'danger')
+        return await async_render_template(
+            'errors/404_error.html', request, 404
+        )
+    return RedirectResponse(request.url_for(
+        'admin_section_detail_page', id=section_id
+    ), status_code=302)
+
+
+@router.post('/section/{section_id}/subsection/{subsection_id}/delete',
+             response_class=HTMLResponse | RedirectResponse)
+@requires('admin')
+async def admin_delete_subsection(
+    request: Request, section_id: int, subsection_id: int
+):
+    base_url = get_env_var('ATHENA_ARES_BASE_URL')
+    try:
+        response = await async_fetch(
+            MethodEnum.delete,
+            url=f'{base_url}/admin/section'
+                f'/{section_id}/subsection/{subsection_id}',
+            headers=TokenRequestHeaders(
+                access_token=request.headers['cookie']
+            )
+        )
+        response.raise_for_status()
+    except HTTPStatusError:
+        if response.content.decode():
+            data = response.json()
+        await flash(request, data.get('detail') or '', AlertTypeEnum.danger)
+        return RedirectResponse(request.url_for(
+            'admin_section_detail_page', id=section_id
+        ), status_code=302)
+    except Exception as exc:
+        await flash(request, f'Unexpected error! - {str(exc)}', 'danger')
+        return await async_render_template(
+            'errors/404_error.html', request, 404
+        )
+    return RedirectResponse(request.url_for(
+        'admin_section_detail_page', id=section_id
+    ), status_code=302)
+
+
+@router.post('/section/{section_id}/subsection/{subsection_id}/patch')
+@requires('admin')
+async def admin_patch_subsection(
+    request: Request,
+    section_id: int,
+    subsection_id: int,
+    form_data: UpdateSubSectionForm = Depends(UpdateSubSectionForm.as_form)
+):
+    data = {}
+    base_url = get_env_var('ATHENA_ARES_BASE_URL')
+    try:
+        response = await async_fetch(
+            MethodEnum.patch,
+            url=f'{base_url}/admin/section'
+                f'/{section_id}/subsection/{subsection_id}',
+            headers=TokenRequestHeaders(
+                access_token=request.headers['cookie']
+            ),
+            json=form_data
+        )
+        response.raise_for_status()
+    except HTTPStatusError:
+        if response.content.decode():
+            data = response.json()
+        await flash(request, data.get('detail') or '', AlertTypeEnum.danger)
+        return RedirectResponse(request.url_for(
+            'admin_subsection_detail_page',
+            section_id=section_id, subsection_id=subsection_id),
+            status_code=302)
+    except Exception as exc:
+        await flash(request, f'Unexpected error! - {str(exc)}', 'danger')
+        return await async_render_template(
+            'errors/404_error.html', request, 404,
+        )
+    return RedirectResponse(request.url_for(
+        'admin_subsection_detail_page',
+        section_id=section_id, subsection_id=subsection_id),
+        status_code=302)
--- apollo/src/routers/home_router.py
+++ apollo/src/routers/home_router.py
@@ -5,7 +5,7 @@ from fastapi.responses import HTMLResponse
 from extensions.base_requests import async_fetch, MethodEnum
 from extensions.env_var import get_env_var
 
-from .base_router import async_render_template, init_section, init_subsection
+from .base_router import async_render_template
 
 router = APIRouter()
 
@@ -13,38 +13,12 @@ router = APIRouter()
 @router.get('/', response_class=HTMLResponse)
 async def home_page(request: Request):
     url = get_env_var('ATHENA_ARES_BASE_URL')
-
-    about_section = await init_section('Sobre')
-    about_subsections = await init_subsection()
-
-    devs_section = await init_section('Devs')
-    devs_subsections = await init_subsection()
-
-    about_section_slug = 'sobre'
     if (response := await async_fetch(
-        MethodEnum.get, f'{url}/section/{about_section_slug}'
+        MethodEnum.get, f'{url}/section', params={'is_visible': True}
     )) and response.is_success:
-        about_section = response.json()
-    if (response := await async_fetch(
-        MethodEnum.get, f'{url}/section/{about_section_slug}/subsection',
-    )) and response.is_success:
-        about_subsections = response.json()
-
-    devs_section_slug = 'devs'
-    if (response := await async_fetch(
-        MethodEnum.get, f'{url}/section/{devs_section_slug}'
-    )) and response.is_success:
-        devs_section = response.json()
-    if (response := await async_fetch(
-        MethodEnum.get, f'{url}/section/{devs_section_slug}/subsection',
-    )) and response.is_success:
-        devs_subsections = response.json()
-
+        sections = response.json()
+    else:
+        sections = []
     return await async_render_template(
         'index_template.html', request,
-        context_request={
-            'about_section': about_section,
-            'about_subsections': about_subsections,
-            'devs_section': devs_section,
-            'devs_subsections': devs_subsections
-        })
+        context_request={'sections': sections})
